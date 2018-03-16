a7191fe29f80069908721d98a2e64c91c4b1ea7f
Martin Gausby <mg@shopgun.com>
2018-03-16 18:03:47 +0100

    Add a docker setup
    I've included a function that will setup the proper docker environment
    for the docker machine by using the output of the `docker-machine env`
    command. I do not know if this is a good and bulletproof way, but it
    seems to do the trick on my work system.

--- config/fundamental-mode.el
+++ config/fundamental-mode.el
@@ -262,3 +262,54 @@ current project root"
          ("C-c C-t" . projectile-test-project)
          ("M-p" . makefile-previous-dependency)
          ("M-n" . makefile-next-dependency))))
+
+;;
+;; docker
+;;
+(use-package docker
+  :if (file-exists-p "~/.docker")
+  :preface
+  (defun mg/update-docker-machine-env (machine-name)
+  "Update the system environment with the information provided by
+the `docker-machine env machine-name' command."
+  (interactive "sMachine name: ")
+  (let* ((docker-machine-env-cmd
+          (format "docker-machine env %s" machine-name))
+         (machine-env-result
+          (shell-command-to-string docker-machine-env-cmd)))
+    (seq-do
+     (lambda (line)
+       ;; we are only concerned with lines starting with export
+       (if (string-prefix-p "export " line)
+           ;; remove "export " and split the line in key/value
+           (let* ((foo (string-remove-prefix "export " line))
+                  (kvpair (split-string-and-unquote foo "=")))
+             ;; update emacs env with docker machine env settings
+             (setenv (car kvpair) (cadr kvpair)))))
+     (split-string machine-env-result "\n" 'strip-empty)))))
+
+;; for editing `Dockerfile`s
+(use-package dockerfile-mode)
+
+;; allow tramping into docker containers
+(use-package docker-tramp
+  :if (file-exists-p "~/.docker")
+  :config
+  (push (cons
+         "docker"
+         '((tramp-login-program "docker")
+           (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+           (tramp-remote-shell "/bin/sh")
+           (tramp-remote-shell-args ("-i") ("-c"))))
+        tramp-methods)
+  (defadvice tramp-completion-handle-file-name-all-completions
+  (around dotemacs-completion-docker activate)
+  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
+    a list of active Docker container names, followed by colons."
+  (if (equal (ad-get-arg 1) "/docker:")
+      (let* ((dockernames-raw (shell-command-to-string "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
+             (dockernames (cl-remove-if-not
+                           #'(lambda (dockerline) (string-match ":$" dockerline))
+                           (split-string dockernames-raw "\n"))))
+        (setq ad-return-value dockernames))
+    ad-do-it)))
